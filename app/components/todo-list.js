46070fedebd6ff5aba2fa88b6935706b625d2786
sanjsanj <sanjsanj@hotmail.com>
2015-08-22 21:23:03 +0100

    can toggle all todos complete or active, clear button now correctly hides when no todos are complete

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -5,8 +5,20 @@ export default Ember.Component.extend({
     createTodo: function(newTitle) {
       this.set('newTitle', '');
       this.sendAction('createTodo', newTitle);
+    },
+    clearCompleted: function() {
+      var completed = this.get('model').filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
+  completed: Ember.computed('model.@each.isCompleted', function() {
+    var model = this.get('model');
+    return model.filterBy('isCompleted', true).get('length');
+  }),
+  hasCompleted: Ember.computed('completed', function() {
+    return this.get('completed') > 0;
+  }),
   remaining: Ember.computed('model.@each.isCompleted', function() {
     var model = this.get('model');
     return model.filterBy('isCompleted', false).get('length');
@@ -14,5 +26,16 @@ export default Ember.Component.extend({
   inflection: Ember.computed('remaining', function() {
     var remaining = this.get('remaining');
     return (remaining === 1) ? 'item' : 'items';
-  })
+  }),
+  allAreDone: function(key, value) {
+    var model = this.get('model');
+    console.log(key + ": " + value);
+    if (value === undefined) {
+      return model.get('length') > 0 && model.isEvery('isCompleted', true);
+    } else {
+      model.setEach('isCompleted', value);
+      model.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted')
 });
